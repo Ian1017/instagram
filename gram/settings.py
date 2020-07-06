2ef99e195e6938223d95727788d354a9a51a3f50
ian1017 <mdawidamengo@gmail.com>
2020-07-07 01:00:35 +0300

    add README

--- gram/settings.py
+++ gram/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z(pod5pnsdo@k(c+@xea_@pr7_cbl^lte1yr=_67hrpkhcrs-n'
+#
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.template.context_
+    'django.template.context_processors.media',
 ]
 
 MIDDLEWARE = [
