dfda2b21a715889a76ad13b2d6fcae1ed3058b1b
ian1017 <mdawidamengo@gmail.com>
2020-07-06 16:05:52 +0300

    Create search form and its url that renders to the search html

--- ig/models.py
+++ ig/models.py
@@ -29,5 +29,5 @@ class Profile(models.Model):
     last_name = models.CharField(max_length=20)
     email = models.EmailField()
     editor = models.ForeignKey(Editor)
-    profile_image = models.ImageField()
+    profile_image = models.ImageField(upload_to = 'profile')
     bio = models.TextField()
