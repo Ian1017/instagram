427484be7ce7651b790e6641fdbd5539ef15f3df
ian1017 <mdawidamengo@gmail.com>
2020-07-06 16:59:50 +0300

    CCreate Post model

--- ig/admin.py
+++ ig/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Profile,tags
+from .models import tags,Profile,Post
 
 # Register your models here.
-
 admin.site.register(Profile)
 admin.site.register(tags) 
+admin.site.register(Post) 
